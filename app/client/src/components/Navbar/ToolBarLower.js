f37fb4f12ccc9b3393ed1c8d09eabbbaac93a2f4
r-drkn <robert.dirken@gmail.com>
2020-12-07 11:46:17 +1000

    Debugged git

---
+++ app/client/src/components/Navbar/ToolBarLower.js
@@ -0,0 +1,50 @@
+import { Link } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import React from "react";
+import SearchField from "./SearchField";
+import Toolbar from "@material-ui/core/Toolbar";
+
+const useStyles = makeStyles((theme) => {
+  const {
+    palette: { secondary },
+    breakpoints,
+  } = theme;
+
+  return {
+    toolBarLower: {
+      [breakpoints.down("sm")]: {
+        display: "none",
+      },
+      height: "6.25vh",
+      justifyContent: "space-between",
+    },
+    navLinks: {
+      color: secondary.main,
+      cursor: "pointer",
+      fontSize: "1.5rem",
+      letterSpacing: 1,
+      marginRight: "3vw",
+      textDecoration: "none",
+    },
+    searchField: {
+      width: "25vw",
+    },
+  };
+});
+
+export default function ToolBarLower() {
+  const classes = useStyles();
+
+  return (
+    <Toolbar className={classes.toolBarLower}>
+      <div>
+        <Link className={classes.navLinks}>new vinyl</Link>
+        <Link className={classes.navLinks}>genres</Link>
+        <Link className={classes.navLinks}>news</Link>
+        <Link className={classes.navLinks}>staff picks</Link>
+        <Link className={classes.navLinks}>contact</Link>
+      </div>
+      <SearchField />
+    </Toolbar>
+  );
+}
