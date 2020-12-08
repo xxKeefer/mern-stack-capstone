9350f03fc8a49719f733fc0e464ad12a52ba2027
Daniel John Keefer <xxkeefer.code@gmail.com>
2020-12-08 15:38:47 +1000

    added blog db with cloudinary upload

--- app/server/api/apiRoutes.js
+++ app/server/api/apiRoutes.js
@@ -5,10 +5,12 @@ const auth = require("./routes/auth");
 const example = require("./routes/example");
 const admin = require("./routes/admin");
 const shop = require("./routes/shop");
+const blog = require("./routes/blog");
 
 router.use("/auth", auth);
 router.use("/example", example);
 router.use("/admin", admin);
 router.use("/shop", shop);
+router.use("/blog", blog);
 
 module.exports = router;
