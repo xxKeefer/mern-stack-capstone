57f0c7c105046f911fa34ea4e686d09997d266a4
Daniel John Keefer <xxkeefer.code@gmail.com>
2020-12-09 13:26:10 +1000

    aded view templates for HTML emails w/ handlebars

--- app/server/controller/mailer.js
+++ app/server/controller/mailer.js
@@ -1,5 +1,7 @@
 require("dotenv").config();
+const path = require("path");
 const nodemailer = require("nodemailer");
+const hbs = require("nodemailer-express-handlebars");
 
 //HELPERS
 const transporter = nodemailer.createTransport({
@@ -10,24 +12,44 @@ const transporter = nodemailer.createTransport({
   },
 });
 
+const handlebarOptions = {
+  viewEngine: {
+    extName: ".handlebars",
+    partialsDir: path.resolve(__dirname, "../views"),
+    defaultLayout: false,
+  },
+  viewPath: path.resolve(__dirname, "../views"),
+  extName: ".handlebars",
+};
+
+transporter.use("compile", hbs(handlebarOptions));
+
 let mailOptions = {
   from: "xxkeefer.test@gmail.com",
   to: "xxkeefer.code@gmail.com",
   subject: "Testing Nodemailer",
-  text: "This is a test. Mic check 1 2, 1 2.",
+  template: "newsletter",
+  context: {
+    text: "This is a test. Mic check 1 2, 1 2.",
+    etc:
+      "other values here and the .hbs file will be able to access them with helpers",
+  },
 };
 
 //EXPORTS
 
 const sendNewsletter = (req, res) => {
-  const { from, to, subject, text } = req.body;
-  transporter.sendMail({ from, to, subject, text }, (err, data) => {
-    if (err) {
-      res.status(500).json({ message: err.message });
-    } else {
-      res.status(200).json({ message: `mail sent to: ${to}` });
+  const { from, to, subject, context } = req.body;
+  transporter.sendMail(
+    { from, to, subject, template: "newsletter", context },
+    (err, data) => {
+      if (err) {
+        res.status(500).json({ message: err.message });
+      } else {
+        res.status(200).json({ message: `mail sent to: ${to}` });
+      }
     }
-  });
+  );
 };
 
 module.exports = { sendNewsletter };
