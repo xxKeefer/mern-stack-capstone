7aaf0957ea6397c66592a6a8719301ca58a81e3c
Daniel John Keefer <xxkeefer.code@gmail.com>
2021-01-10 10:14:58 +1000

    added cors to backend

--- app/client/src/util/fetch.js
+++ app/client/src/util/fetch.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const publicFetch = axios.create({
-  baseURL: "localhost:8080",
+  baseURL: "http://localhost:8080",
 });
 
 export { publicFetch };
--- app/server/server.js
+++ app/server/server.js
@@ -1,6 +1,7 @@
 if (process.env.NODE_ENV !== "production") require("dotenv").config();
 const path = require("path");
 const express = require("express");
+const cors = require("cors");
 const mongoose = require("mongoose");
 const session = require("express-session");
 const MongoStore = require("connect-mongo")(session);
@@ -9,6 +10,7 @@ const passport = require("passport");
 // EXPRESS CONFIG
 const app = express();
 const port = process.env.PORT || 8080;
+app.use(cors());
 app.use(express.urlencoded({ limit: "50mb", extended: false }));
 app.use(express.json({ limit: "50mb" }));
 
