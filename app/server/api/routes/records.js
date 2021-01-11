b253a16215a268b0dd24eadbc5a66ea17c2a8d14
Daniel John Keefer <xxkeefer.code@gmail.com>
2021-01-11 17:27:10 +1000

    readY to take dynamic queries @ "/api/record/:catagory/:query

--- app/server/api/routes/records.js
+++ app/server/api/routes/records.js
@@ -1,9 +1,16 @@
 const express = require("express");
 let router = express.Router();
 
-const { query, sendCompactDB } = require("../../controller/records");
+const {
+  query,
+  complexQuery,
+  sendCompactDB,
+} = require("../../controller/records");
 
-router.route("/").post(query);
+//router.route("/genre/Jazz").post(query);
+router.route("/:category/:query").get(query);
+
+router.route("/complex/:queryObj").post(complexQuery);
 
 router.route("/search").get(sendCompactDB);
 
