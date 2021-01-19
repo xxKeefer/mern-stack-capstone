6589f014791e25d351682ed3beb5e5de830d3cbd
xxKeefer <xxkeefer.code@gmail.com>
2021-01-19 12:24:45 +1000

    test deploying with urlencoding false

--- app/server/server.js
+++ app/server/server.js
@@ -30,7 +30,7 @@ const corsConfig = {
   credentials: true,
 };
 app.use(cors(corsConfig));
-app.use(express.urlencoded({ limit: "50mb", extended: true }));
+app.use(express.urlencoded({ limit: "50mb", extended: false }));
 app.use(express.json({ limit: "50mb" }));
 
 //DATABASE
